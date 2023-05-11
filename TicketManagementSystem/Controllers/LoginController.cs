626d163c60790f71aef56a25fb33499043a7a92f
NTTu <tudeyer@gmail.com>
2023-05-11 14:09:55 +0700

    11/05

---
+++ TicketManagementSystem/Controllers/LoginController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using TicketManagementSystem.Data;
+using TicketManagementSystem.Models;
+
+namespace TicketManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly TicketManagementSystemContext _context;
+        private readonly IConfiguration _configuration;
+
+        public LoginController(TicketManagementSystemContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        [HttpPost("Login")]
+        public IActionResult Login(String userMail, String userPassword)
+        {
+            Models.User user = _context.Users.Where(s => s.Mail == userMail 
+            && s.UserPassword == userPassword).FirstOrDefault();
+            if (user == null)
+            {
+                return NotFound();
+            }
+            var result = new {success = true, token = GenerateToken(user) };
+            return Ok(result);
+        }
+
+        private String GenerateToken(Models.User user)
+        {
+            var authClaims = new List<Claim>
+            {
+                new Claim("userMail", user.Mail),
+                new Claim("userName", "Admin01"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+            var authSignInKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:SecretKey"]));
+            var token = new JwtSecurityToken(
+                expires: DateTime.Now.AddDays(1),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSignInKey, SecurityAlgorithms.HmacSha256Signature)
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
--- TicketManagementSystem/Startup.cs
+++ TicketManagementSystem/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -7,10 +8,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using TicketManagementSystem.Data;
 
@@ -34,7 +37,16 @@ namespace TicketManagementSystem
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "TicketManagementSystem", Version = "v1" });
             });
-            //services.AddDbContext<TicketManagementSystemContext>(option => option.UseModel)
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(option =>
+                {
+                    option.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                    {
+                        ValidateAudience = false,
+                        ValidateIssuer = false,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:SecretKey"]))
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +62,8 @@ namespace TicketManagementSystem
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
