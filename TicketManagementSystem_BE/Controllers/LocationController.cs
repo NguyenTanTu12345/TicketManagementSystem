72146e9a0b9954974a132e40251fd9b29ba52979
NTTu <tudeyer@gmail.com>
2023-05-29 14:06:19 +0700

    29/05

---
+++ TicketManagementSystem_BE/Controllers/LocationController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TicketManagementSystem_BE.Data;
+using TicketManagementSystem_BE.Models;
+
+namespace TicketManagementSystem_BE.Controllers
+{
+    [EnableCors("myOrigins")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        private readonly TicketManagementSystemContext _context;
+
+        public LocationController(TicketManagementSystemContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Location>>> GetAll()
+        {
+            if (_context.Locations == null)
+            {
+                return NotFound();
+            }
+            return await _context.Locations.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Location>> Get(string id)
+        {
+            var location = await _context.Locations.FindAsync(id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+            return location;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Location location)
+        {
+            await _context.Locations.AddAsync(location);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(Get), new { id = location.LocationId }, location);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Edit(Location location)
+        {
+            _context.Entry(location).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(Get), new { id = location.LocationId }, location);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(string id)
+        {
+            var location = await _context.Locations.FindAsync(id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+            _context.Locations.Remove(location);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
